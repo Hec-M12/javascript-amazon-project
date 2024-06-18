3a7f5c4188075bb1c76555594264177a91fd836b
Hector Mejia <hector.mejialr@gmail.com>
2024-06-17 19:11:27 -0600

    Convert load products from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,21 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentsummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import "../data/backend-practice.js";
 
 Promise.all([
-  new Promise((resolve)=>{
-    loadProducts(()=>{
-      resolve('value1');
-    });
-  }),
-  new Promise((resolve)=>{
-    loadCart(()=>{
-      resolve('value2');
-    })
-  })
+  loadProductsFetch(),
 ]).then((values)=>{
   console.log(values);
   renderOrderSummary();
