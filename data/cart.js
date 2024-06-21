8dddb8d065a00e3a85c5fbb5137643c4d2c81682
Hector Mejia <hector.mejialr@gmail.com>
2024-06-20 19:17:49 -0600

    Made the cart empty if not found in local storage

--- data/cart.js
+++ data/cart.js
@@ -4,16 +4,6 @@ getCartLocalStorage();
 export function getCartLocalStorage(){
   if (!localStorage.getItem('cart')){
     cart = [
-    {
-      id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1'
-    },
-    {
-      id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 2,
-      deliveryOptionId: '2'
-    }
     ];
   } else{
     cart = JSON.parse(localStorage.getItem('cart'))
